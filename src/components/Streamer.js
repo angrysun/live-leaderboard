555c1117642d0bfd101c07ec42771db517987840
Steven Vasquez <s.vasquez85@gmail.com>
2022-06-30 10:35:42 +0900

    created API call

--- src/components/Streamer.js
+++ src/components/Streamer.js
@@ -8,6 +8,16 @@ export default function Streamer() {
   }, []);
 
   const getStreamer = () => {
-
+    fetch('https://webcdn.17app.co/campaign/pretest/data.json', {
+      method: 'GET',
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data);
+        setStreamer(data);
+      })
   }
 }
